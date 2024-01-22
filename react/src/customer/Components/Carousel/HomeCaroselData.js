d10b69e4f89160ec7b4259d64e7fcd27e5fa0f40
PRATIKBAGADE1 <pratikbagade13@gmail.com>
2024-01-22 15:20:36 +0530

    Ecommerse website edited code

---
+++ react/src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,24 @@
+export const homeCarouselData=[
+    {
+        image:"https://www.lavanyathelabel.com/cdn/shop/files/weddddding_0deb8aad-4f81-4ffd-8f73-22d1670bf587.jpg?v=1700120957&width=1400",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.lavanyathelabel.com/cdn/shop/files/saree_pc_1b94c0b4-e87d-4cba-a195-ae4ff16506e9.jpg?v=1700729588&width=1400",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:" https://www.lavanyathelabel.com/cdn/shop/files/lehenga_pc_1f62cf1e-a0db-4643-9169-0a9a95e63b10.jpg?v=1700729588&width=1400",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.lavanyathelabel.com/cdn/shop/files/ethnc_pc.jpg?v=1700729588&width=1400",
+        path:"/women/clothing/women_saree"
+    },
+    {
+       image:" https://www.soch.com/media/wysiwyg/HB_banner_Desktop_21.jpg",
+       path:"/women/clothing/women_dress"
+    }
+
+]
+
---
+++ react/src/customer/Components/footer/Footer.jsx
@@ -0,0 +1,88 @@
+import { Grid, Link, Typography } from '@mui/material';
+
+const Footer = () => {
+  return (
+    <Grid className='bg-black text-white mt-10 text-center' container color={'white' } sx={{ bgcolor: 'black', color: 'white', py: 3 }}>
+      <Grid  item xs={12} sm={6} md={3}>
+        <Typography className='pb-5' variant="h6" gutterBottom>
+          Company
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          About
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Blog
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Jobs
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Press
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Partners
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography className='pb-5' variant="h6" gutterBottom>
+          Solutions
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Marketing
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Analytics
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Commerce
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Insights
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Support
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography className='pb-5' variant="h6" gutterBottom>
+          Documentation
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Guides
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          API Status
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography className='pb-5' variant="h6" gutterBottom>
+          Legal
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Claim
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Privacy
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Terms
+        </Typography>
+      </Grid>
+      <Grid className='pt-20' item xs={12} >
+        <Typography variant="body2" component="p" align="center">
+          &copy; 2023 @Gcoej. All rights reserved.
+        </Typography>
+        <Typography variant="body2" component="p" align="center">
+          Made with love by LY Boys (Computer Engineer).
+        </Typography>
+        <Typography variant="body2" component="p" align="center">
+        Developer :- 1)Pratik Bagade 2)Dipak Chahakar 3)Yash Deshmukh
+          
+          
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Footer;
