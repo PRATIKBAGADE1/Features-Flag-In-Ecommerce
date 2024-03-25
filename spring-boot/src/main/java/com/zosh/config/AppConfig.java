9f13c65f01b0b94ce8e7a84f6c3831f4009ff81e
Pratik Bagade <99264531+PRATIKBAGADE1@users.noreply.github.com>
2024-03-25 20:12:51 +0530

    Update AppConfig.java
    adding deployed website domain link in to the backend code

--- spring-boot/src/main/java/com/zosh/config/AppConfig.java
+++ spring-boot/src/main/java/com/zosh/config/AppConfig.java
@@ -43,7 +43,7 @@ public class AppConfig {
 								"http://localhost:3000",        //react
 								"http://localhost:4000",
 								"http://localhost:4200",            //angular
-								""
+								"https://ydpmart.vercel.app/"
 								
 							)
 						);
